dc74fe9e9c2826c293dceeae5ec4c53acd5ecbba
Apurbo-hue <labunna123@gmail.com>
2026-07-04 22:04:48 +0600

    delete car functionality added

--- src/Pages/MyListings/MyListingsTable.jsx
+++ src/Pages/MyListings/MyListingsTable.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import Swal from 'sweetalert2';
+import useAxios from '../../../hooks/useAxios';
 
-const MyListingsTable = ({cars}) => {
+const MyListingsTable = ({ cars }) => {
+    const axiosInstance = useAxios();
+
+    const handleDelete = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosInstance.delete(`/cars/${id}`)
+                    .then(data => {
+                        if (data.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+
+        });
+    }
     return (
         <div className="overflow-x-auto rounded-xl border border-base-300 shadow-lg">
             <table className="table">
@@ -71,7 +102,7 @@ const MyListingsTable = ({cars}) => {
                                         Update
                                     </button>
 
-                                    <button className="btn btn-sm btn-error text-white">
+                                    <button onClick={() => handleDelete(car._id)} className="btn btn-sm btn-error text-white">
                                         Delete
                                     </button>
 
